e8d4b657f106235845d929bdff6febf28218df9d
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-13 11:21:02 -0500

    Adding in GUI pages

---
+++ src/Controllers/MainMenuController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+    public Button mainCustomerButton;
+    public Button mainAppointmentButton;
+    public Button mainReportsButton;
+    public Button mainLogsButton;
+
+    public void mainCustomerButton(ActionEvent actionEvent) {
+    }
+
+    public void mainAppointmentButton(ActionEvent actionEvent) {
+    }
+
+    public void mainReportsButton(ActionEvent actionEvent) {
+    }
+
+    public void mainLogsButton(ActionEvent actionEvent) {
+    }
+}
