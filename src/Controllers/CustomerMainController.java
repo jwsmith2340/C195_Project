f74b081a9f609d253467b7527a4ddeddfd2c12e3
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-13 20:48:47 -0500

    Creating first SQL query in Customer Main

--- src/Controllers/CustomerMainController.java
+++ src/Controllers/CustomerMainController.java
@@ -1,26 +1,74 @@
 package Controllers;
 
+import Models.Customer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import Database.DBConnection;
 
-public class CustomerMainController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class CustomerMainController implements Initializable {
+
+    @FXML
+    public TableView customerMainTableView;
+    @FXML
     public TableColumn customerIdColumn;
+    @FXML
     public TableColumn customerNameColumn;
+    @FXML
     public TableColumn customerAddressColumn;
+    @FXML
     public TableColumn customerPostalColumn;
+    @FXML
     public TableColumn customerCountryColumn;
+    @FXML
     public TableColumn customerDivisionColumn;
+    @FXML
     public Button customersAddButton;
+    @FXML
     public Button customersModifyButton;
+    @FXML
     public Button customersBackButton;
+    @FXML
+    ObservableList<Customer> customerList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Customer main page initialized");
+
+        String sqlStatement = "SELECT Customer_ID, Customer_Name, Address, Postal_Code, Phone, Country, Division\n" +
+                "FROM CUSTOMERS \n" +
+                "INNER JOIN first_level_divisions \n" +
+                "INNER JOIN countries\n" +
+                "WHERE customers.Division_ID = first_level_divisions.Division_ID\n" +
+                "AND first_level_divisions.Country_ID = countries.Country_ID;";
+        try {
+            PreparedStatement sqlPreparedStatement = DBConnection.startConnection().prepareStatement(sqlStatement);
+            ResultSet sqlResult = sqlPreparedStatement.executeQuery(sqlStatement);
+            System.out.println(sqlResult);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void customersAddButton(ActionEvent actionEvent) throws IOException {
         Parent add_product = FXMLLoader.load(getClass().getResource("/Views/AddCustomer.fxml"));
@@ -45,4 +93,5 @@ public class CustomerMainController {
         addPartStage.setScene(addPartScene);
         addPartStage.show();
     }
+
 }
---
+++ src/Models/Customer.java
@@ -0,0 +1,68 @@
+package Models;
+
+public class Customer {
+
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+    private int customerPostal;
+    private String customerCountry;
+    private String customerDivision;
+
+    public Customer(int customerId, String customerName, String customerAddress, int customerPostal, String customerCountry, String customerDivision) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerPostal = customerPostal;
+        this.customerCountry = customerCountry;
+        this.customerDivision = customerDivision;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public int getCustomerPostal() {
+        return customerPostal;
+    }
+
+    public void setCustomerPostal(int customerPostal) {
+        this.customerPostal = customerPostal;
+    }
+
+    public String getCustomerCountry() {
+        return customerCountry;
+    }
+
+    public void setCustomerCountry(String customerCountry) {
+        this.customerCountry = customerCountry;
+    }
+
+    public String getCustomerDivision() {
+        return customerDivision;
+    }
+
+    public void setCustomerDivision(String customerDivision) {
+        this.customerDivision = customerDivision;
+    }
+}
