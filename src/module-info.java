19ff0bf04d0717dbff28f6583c8d6cda7e223e83
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-13 21:28:03 -0500

    Exporting Models

--- src/module-info.java
+++ src/module-info.java
@@ -8,6 +8,6 @@ module c195.c195 {
     opens main to javafx.fxml;
     exports Controllers;
     opens Controllers to javafx.fxml;
-//    exports Model;
-//    opens Model to javafx.fxml;
+    exports Models;
+    opens Model to javafx.fxml;
 }
