069a0b8d47737adcb0113e14794d58d0ec69d650
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-15 16:55:14 -0500

    Redirecting to customer main, syncing commit for VM.

--- src/Controllers/MainMenuController.java
+++ src/Controllers/MainMenuController.java
@@ -20,7 +20,7 @@ public class MainMenuController {
 
     public void mainCustomerButton(ActionEvent actionEvent) throws IOException {
 //        Parent add_product = FXMLLoader.load(getClass().getResource("/Views/CustomerMain.fxml"));
-        Parent add_product = FXMLLoader.load(getClass().getResource("/Views/AddCustomer.fxml"));
+        Parent add_product = FXMLLoader.load(getClass().getResource("/Views/CustomerMain.fxml"));
         Scene addPartScene = new Scene(add_product);
         Stage addPartStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         addPartStage.setScene(addPartScene);
