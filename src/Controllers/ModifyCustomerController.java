a5dae90af177af2643264598c7eb234ae174ced4
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-15 21:19:02 -0500

    Customer update logic in place, ready for testing in VM

--- src/Controllers/ModifyCustomerController.java
+++ src/Controllers/ModifyCustomerController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Database.DBConnection;
+import Database.DBPreparedStatement;
 import Models.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ModifyCustomerController {
     @FXML
@@ -34,8 +38,52 @@ public class ModifyCustomerController {
 
     Customer modifyCustomer;
 
-    public void modifyCustomerSave(ActionEvent actionEvent) {
+    public void modifyCustomerSave(ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
         System.out.println("Modify Customer Save Button");
+        java.util.Date datetime = new java.util.Date();
+        java.text.SimpleDateFormat dateTimeFormatted = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        String currentTime = dateTimeFormatted.format(datetime);
+
+        Integer customerId = Integer.valueOf(modifyCustomerId.getText());
+        String customerName = String.valueOf(modifyCustomerNameField.getText());
+        String customerAddress = String.valueOf(modifyCustomerAddressField.getText());
+        String customerZip = String.valueOf(modifyCustomerZipField.getText());
+        String customerPhone = String.valueOf(modifyCustomerPhoneField.getText());
+        String customerCountry = String.valueOf(modifyCustomerCountryCombo.getValue());
+        String customerDivision = String.valueOf(modifyCustomerDivisionCombo.getValue());
+
+        String sqlUpdateStatement = "UPDATE customers SET Customer_Name = ?, Address = ?, Postal_Code = ?, Phone = ?, " +
+                "Last_Update = ?, Last_Updated_By = ?, Division_ID = ? WHERE customer_id = ?";
+
+        DBPreparedStatement.setPreparedStatement(DBConnection.startConnection(), sqlUpdateStatement);
+        PreparedStatement preparedStatement = DBPreparedStatement.getPreparedStatement();
+
+        preparedStatement.setString(1, customerName);
+        preparedStatement.setString(2, customerAddress);
+        preparedStatement.setString(3, customerZip);
+        preparedStatement.setString(4, customerPhone);
+        preparedStatement.setString(5, currentTime);
+        preparedStatement.setString(6, "updating user LOGIC NEEDED");
+        preparedStatement.setInt(7, 66);
+        preparedStatement.setInt(8, customerId);
+        // This logic needs updated to parse the country/division info for validation and then return int div_id value
+
+        try {
+            preparedStatement.execute();
+            if (preparedStatement.getUpdateCount() > 0) {
+                System.out.println("Number of rows affected: " + preparedStatement.getUpdateCount());
+                Parent add_product = FXMLLoader.load(getClass().getResource("/Views/CustomerMain.fxml"));
+                Scene addPartScene = new Scene(add_product);
+                Stage addPartStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                addPartStage.setScene(addPartScene);
+                addPartStage.show();
+            } else {
+                System.out.println("An error occurred and the customer wasn't updated.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
