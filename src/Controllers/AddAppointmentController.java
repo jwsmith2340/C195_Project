a369c41d28bbb1149f9b38cc0e459e43f20239d4
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-17 15:43:30 -0500

    Working on dat/time formatting

--- src/Controllers/AddAppointmentController.java
+++ src/Controllers/AddAppointmentController.java
@@ -160,51 +160,52 @@ public class AddAppointmentController implements Initializable {
 
         Contact contactIDCall = new Contact();
         int contactID = contactIDCall.getContactId(appointmentContact);
-
-        LocalTime appointmentFormattedStartTime = LocalTime.from(LocalDateTime.parse(appointmentStartTime, timeFormatter));
-        LocalTime appointmentFormattedEndTime = LocalTime.from(LocalDateTime.parse(appointmentEndTime, timeFormatter));
-
-        LocalTime appointmentFormattedStart = LocalTime.from(LocalDateTime.of(appointmentDate, appointmentFormattedStartTime));
-        LocalTime appointmentFormattedEnd = LocalTime.from(LocalDateTime.of(appointmentDate, appointmentFormattedEndTime));
-
-        System.out.println(appointmentCustomerId);
-        System.out.println(appointmentUserId);
-
-        String sqlInsertStatement = "INSERT INTO Appointments (Title, Description, Location, Type, Start, End, Create_Date," +
-                " Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
-        DBPreparedStatement.setPreparedStatement(DBConnection.startConnection(), sqlInsertStatement);
-        PreparedStatement preparedStatement = DBPreparedStatement.getPreparedStatement();
-
-        preparedStatement.setString(1, appointmentTitle);
-        preparedStatement.setString(2, appointmentDescription);
-        preparedStatement.setString(3, appointmentLocation);
-        preparedStatement.setString(4, appointmentType);
-        preparedStatement.setString(5, String.valueOf(appointmentFormattedStart));
-        preparedStatement.setString(6, String.valueOf(appointmentFormattedEnd));
-        preparedStatement.setString(7, currentTime);
-        preparedStatement.setString(8, "Whoever made it");
-        preparedStatement.setString(9, currentTime);
-        preparedStatement.setString(10, "Whoever updated it");
-        preparedStatement.setInt(11, appointmentCustomerId);
-        preparedStatement.setInt(12, appointmentUserId);
-        preparedStatement.setInt(13, contactID);
-
-        try {
-            preparedStatement.execute();
-            if (preparedStatement.getUpdateCount() > 0) {
-                System.out.println("Number of rows affected: " + preparedStatement.getUpdateCount());
-                Parent add_product = FXMLLoader.load(getClass().getResource("/Views/AppointmentMain.fxml"));
-                Scene addPartScene = new Scene(add_product);
-                Stage addPartStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-                addPartStage.setScene(addPartScene);
-                addPartStage.show();
-            } else {
-                System.out.println("An error occurred and no customers were created.");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println(appointmentDate);
+//        String startDateRaw = appointmentDate +
+//
+//
+//
+//        LocalTime appointmentFormattedStart =
+//        LocalTime appointmentFormattedEnd = LocalTime.from(LocalDateTime.of(appointmentDate, appointmentFormattedEndTime));
+//
+//        System.out.println(appointmentCustomerId);
+//        System.out.println(appointmentUserId);
+//
+//        String sqlInsertStatement = "INSERT INTO Appointments (Title, Description, Location, Type, Start, End, Create_Date," +
+//                " Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+//
+//        DBPreparedStatement.setPreparedStatement(DBConnection.startConnection(), sqlInsertStatement);
+//        PreparedStatement preparedStatement = DBPreparedStatement.getPreparedStatement();
+//
+//        preparedStatement.setString(1, appointmentTitle);
+//        preparedStatement.setString(2, appointmentDescription);
+//        preparedStatement.setString(3, appointmentLocation);
+//        preparedStatement.setString(4, appointmentType);
+//        preparedStatement.setString(5, String.valueOf(appointmentFormattedStart));
+//        preparedStatement.setString(6, String.valueOf(appointmentFormattedEnd));
+//        preparedStatement.setString(7, currentTime);
+//        preparedStatement.setString(8, "Whoever made it");
+//        preparedStatement.setString(9, currentTime);
+//        preparedStatement.setString(10, "Whoever updated it");
+//        preparedStatement.setInt(11, appointmentCustomerId);
+//        preparedStatement.setInt(12, appointmentUserId);
+//        preparedStatement.setInt(13, contactID);
+//
+//        try {
+//            preparedStatement.execute();
+//            if (preparedStatement.getUpdateCount() > 0) {
+//                System.out.println("Number of rows affected: " + preparedStatement.getUpdateCount());
+//                Parent add_product = FXMLLoader.load(getClass().getResource("/Views/AppointmentMain.fxml"));
+//                Scene addPartScene = new Scene(add_product);
+//                Stage addPartStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+//                addPartStage.setScene(addPartScene);
+//                addPartStage.show();
+//            } else {
+//                System.out.println("An error occurred and no customers were created.");
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
 
     }
 
