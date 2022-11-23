38f2594cf9a19949b828154f6e32c5c9bcfb21e7
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-23 18:27:04 -0500

    javadoc for user class

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Models","l":"Month"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Models","l":"User"}];updateSearchResults();
--- src/Models/User.java
+++ src/Models/User.java
@@ -1,5 +1,8 @@
 package Models;
 
+/**
+ * The User class allows for global static values to be set so the logged in user name can be accessed everywhere.
+ */
 public class User {
     public static String userName;
 
@@ -7,10 +10,18 @@ public class User {
         return userName;
     }
 
+    /**
+     * Sets the static username
+     * @param userName
+     */
     public static void setUserName(String userName) {
         User.userName = userName;
     }
 
+    /**
+     * The user constructor, takes in the name and creates a simple user object
+     * @param userName
+     */
     public User(String userName) {
         this.userName = userName;
     }
