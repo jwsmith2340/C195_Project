4dbf79f6e946c614f0f012704892f10f8f653dd4
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-22 22:59:57 -0500

    Working on formatting utc to local

--- src/main/HelloApplication.java
+++ src/main/HelloApplication.java
@@ -32,36 +32,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, ParseException {
-//        launch();
-//        DBConnection.closeConnection();
-
-        String startDateFormatted = "2022-11-22 08:30:00";
-        String UTC_STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
-        LocalDateTime localStartDateTime = LocalDateTime.parse(startDateFormatted, DateTimeFormatter.ofPattern(UTC_STANDARD_FORMAT));
-        ZonedDateTime systemStartZonedDateTime = localStartDateTime.atZone(ZoneId.systemDefault());
-        ZonedDateTime utcSqlStartTime = systemStartZonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
-        Instant utcSqlStartTimestamp = utcSqlStartTime.toInstant();
-        String utcSqlStartTimestampString = String.valueOf(utcSqlStartTimestamp);
-        String utcSqlStartSubString = utcSqlStartTimestampString.substring(0,19);
-
-        String[] startTime = utcSqlStartSubString.split("T");
-        String startTimeFull = startTime[0] + " " + startTime[1];
-        //
-        //
-        LocalDateTime localStartDateTimeee = LocalDateTime.parse(startTimeFull, DateTimeFormatter.ofPattern(UTC_STANDARD_FORMAT));
-        ZonedDateTime systemStartZonedDateTimeee = localStartDateTimeee.atZone(ZoneId.of("UTC"));
-
-
-        ZonedDateTime utcSqlStartTimeee = systemStartZonedDateTimeee.withZoneSameInstant(ZoneId.systemDefault());
-        System.out.println("SHould be good here vv");
-        System.out.println(utcSqlStartTimeee);
-
-        String localTimeString = String.valueOf(utcSqlStartTimeee);
-        String localTimeSubString = localTimeString.substring(0,16);
-        String[] localTime = localTimeSubString.split("T");
-        String formattedDateTimeFull = localTime[0] + " " + localTime[1] + ":00";
-        System.out.println(formattedDateTimeFull);
+        launch();
+        DBConnection.closeConnection();
     }
 
 }
