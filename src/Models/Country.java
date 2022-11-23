5f89012916db9a846f89751cb787ac08053bee14
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-23 18:20:43 -0500

    The Country class was updated, had lost the appointment numbers in the country table of reports, reverted back.

--- src/Models/Country.java
+++ src/Models/Country.java
@@ -1,19 +1,36 @@
 package Models;
 
+/**
+ * The country class creates instances of the country class
+ */
 public class Country {
 
     private final int apptTotal;
     private final String country;
 
+    /**
+     * The country constructor takes in the total number of appointments and the name of the country, this is
+     * used to populate table views in the reports class
+     * @param apptTotal
+     * @param country
+     */
     public Country(int apptTotal, String country) {
         this.apptTotal = apptTotal;
         this.country = country;
     }
 
+    /**
+     * returns the number of appointments in an instance of the country class
+     * @return
+     */
     public int getApptTotal() {
         return apptTotal;
     }
 
+    /**
+     * returns the name of country in an instance of the Country class
+     * @return
+     */
     public String getCountry() {
         return country;
     }
