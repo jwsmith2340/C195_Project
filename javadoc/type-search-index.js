aba4a83c52bae5f82985b8a904c2397ad174fc1b
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-23 17:55:00 -0500

    javadoc for db prepared statement created

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Database","l":"DBConnection"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Database","l":"DBPreparedStatement"}];updateSearchResults();
--- src/Database/DBPreparedStatement.java
+++ src/Database/DBPreparedStatement.java
@@ -4,14 +4,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * The prepared statement exists to create prepared statements to declutter code throughout the application
+ */
 public class DBPreparedStatement {
 
     private static PreparedStatement preparedStatement;
 
+    /**
+     * The setPreparedStatement method takes in the connection and the sql statement to create a prepared statement
+     * with a valid connection to the DB. This method greatly reduces code clutter throughout the application.
+     * @param conn
+     * @param sqlStatement
+     * @throws SQLException
+     */
     public static void setPreparedStatement(Connection conn, String sqlStatement) throws SQLException {
         preparedStatement = conn.prepareStatement(sqlStatement);
     }
 
+    /**
+     * A simple getter method for the prepared statement.
+     * @return
+     */
     public static PreparedStatement getPreparedStatement() {
         return preparedStatement;
     }
