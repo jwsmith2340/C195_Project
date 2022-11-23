97d82211cd05c64ab92f409e7cde58c3e7801672
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-11-23 14:50:21 -0500

    javadocs created, code cleaned up

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"Controllers","l":"AddAppointmentController"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
+typeSearchIndex = [{"p":"Controllers","l":"AddCustomerController"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
--- src/Controllers/AddCustomerController.java
+++ src/Controllers/AddCustomerController.java
@@ -17,21 +17,21 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
-import java.util.TimeZone;
 
+/**
+ * AddCustomerController handles adding a new customer in the AddCustomer view. User input is taken and a
+ * new customer is generated after input validation is performed.
+ */
 public class AddCustomerController implements Initializable {
     @FXML
     public ComboBox<String> addCustomerCountryCombo;
@@ -52,15 +52,27 @@ public class AddCustomerController implements Initializable {
     @FXML
     ObservableList<String> divisionList = FXCollections.observableArrayList();
 
+    /**
+     * Initialize sets the country combo box. The division combo box is not set here because its values
+     * based on the country selected
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setCountryComboBox();
-//        setDivisionComboBox();
     }
 
+    /**
+     * addCustomerSave first gets the local time and converts it to UTC for the DB. All text fields and combo box values
+     * are then assigned to variables, and null checks are performed to ensure user input. Validation is performed, and
+     * if validation passes, an INSERT statement is run to create a new customer. The user is then redirected to the
+     * main customer view.
+     * @param actionEvent
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public void addCustomerSave(ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
-        java.util.Date datetime = new java.util.Date();
-        java.text.SimpleDateFormat dateTimeFormatted = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         ZoneId utcZoneId = ZoneId.of("UTC");
         LocalDateTime utcDateTime = LocalDateTime.now(utcZoneId);
@@ -73,9 +85,6 @@ public class AddCustomerController implements Initializable {
         String customerPhone = String.valueOf(addCustomerPhoneField.getText());
         String customerCountry = String.valueOf(addCustomerCountryCombo.getValue());
         String customerDivision = String.valueOf(addCustomerDivisionCombo.getValue());
-
-        System.out.println(customerCountry);
-        System.out.println(customerDivision);
 
         if (customerCountry != "null") {
 
@@ -131,6 +140,11 @@ public class AddCustomerController implements Initializable {
 
     }
 
+    /**
+     * addCustomerCancel redirects the user to the CustomerMain view without saving any input.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void addCustomerCancel(ActionEvent actionEvent) throws IOException {
         Parent add_product = FXMLLoader.load(getClass().getResource("/Views/CustomerMain.fxml"));
         Scene addPartScene = new Scene(add_product);
@@ -139,6 +153,10 @@ public class AddCustomerController implements Initializable {
         addPartStage.show();
     }
 
+    /**
+     * setCountryComboBox is run when the AddCustomer view is visited. A query is made to the db to get all countries,
+     * and that data then populates the countryList, which is then used to populate the country combo box.
+     */
     private void setCountryComboBox() {
         String sqlStatement = "SELECT country FROM countries;";
         try {
@@ -156,8 +174,15 @@ public class AddCustomerController implements Initializable {
 
         addCustomerCountryCombo.getItems().clear();
         addCustomerCountryCombo.getItems().addAll(countryList);
+        // addCustomerCountryCombo.getItems().add()     GOOD CANDIDATE FOR 2ND LAMBDA
     }
 
+    /**
+     * setDivisionComboBox is called whenever a country is selected from the country combo box. Once a country is
+     * selected, a query is run on the first_level_divisions table where that country's ID matches that division.
+     * The division combo box is then populated with those values.
+     * @param Country_ID
+     */
     private void setDivisionComboBox(int Country_ID) {
         divisionList.clear();
         String sqlStatement = "SELECT division FROM first_level_divisions WHERE Country_ID = ?;";
@@ -184,13 +209,13 @@ public class AddCustomerController implements Initializable {
         addCustomerDivisionCombo.getItems().addAll(divisionList);
     }
 
-    private int getDivisionId() {
-        return 1;
-    }
-
+    /**
+     * When a country is selected, this method is run. It selects the country ID for use in the setDivisionComboBox
+     * method. It also enables the division Combo Box, which had been disabled until a country was selected.
+     * @param actionEvent
+     */
     public void addCustomerCountryCombo(ActionEvent actionEvent) {
         String countryName = addCustomerCountryCombo.getSelectionModel().getSelectedItem();
-        System.out.println(countryName);
         String sqlStatement = "SELECT Country_ID FROM Countries WHERE Country = ?;";
 
         try {
@@ -218,6 +243,14 @@ public class AddCustomerController implements Initializable {
 
     }
 
+    /**
+     * This method provides validation for user input.
+     * @param customerName
+     * @param customerAddress
+     * @param customerPostal
+     * @param customerPhone
+     * @return
+     */
     public boolean customerFieldTypeValidation(String customerName, String customerAddress, String customerPostal, String customerPhone) {
 
         boolean validationResult = true;
@@ -246,6 +279,10 @@ public class AddCustomerController implements Initializable {
 
     }
 
+    /**
+     * All error alerts are set with appropriate values to declutter the other methods.
+     * @param errorCode
+     */
     private void errorAlert(int errorCode) {
         if(errorCode == 1) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
